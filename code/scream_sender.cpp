a51e1583208cc6d9c5ea9eb3f4673fe436764a29
Nawel Alioua <nawel@ucsb.edu>
2025-10-07 17:28:23 -0700

    Add a print to output the target bitrate

--- code/scream_sender.cpp
+++ code/scream_sender.cpp
@@ -352,6 +352,8 @@ void* createRtpThread(void* arg) {
 		uint32_t ts = (uint32_t)(time_ntp / 65536.0 * 90000);
 		float rateTx = screamTx->getTargetBitrate(time_ntp, SSRC) * rateScale;
 
+		cout << "SCReAM target bitrate: " << rateTx / 1000 << " kbps" << endl;
+
 		mtu = screamTx->getRecommendedMss(time_ntp);
 
 		screamTx->setCwndMinLow((mtu+12)*2);

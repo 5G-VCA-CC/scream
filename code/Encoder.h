eaab02ea3f081900ae94063c9545111524237953
Nawel Alioua <nawel@ucsb.edu>
2026-03-31 16:36:51 -0700

    Force a keyframe when getTargetBitrate returns -1.0 This is following a recommendation in the original code base.

--- code/Encoder.h
+++ code/Encoder.h
@@ -22,7 +22,8 @@ public:
   bool encode_next_frame(uint32_t target_bitrate_bps,
                          int mtu,
                          std::vector<std::vector<uint8_t>>& payloads,
-                         bool* is_key_frame = nullptr);
+                         bool* is_key_frame = nullptr,
+                         bool force_key_frame = false);
 
   uint16_t width() const { return width_; }
   uint16_t height() const { return height_; }
